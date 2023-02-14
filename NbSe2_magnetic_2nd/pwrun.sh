9f4a3415309a1691fce79aff3ce1781f79c4ea7e
honey_badger_td <47205931+hhoneybadger@users.noreply.github.com>
2023-02-14 22:12:40 +0600

    Update pwrun.sh

--- NbSe2_magnetic_2nd/pwrun.sh
+++ NbSe2_magnetic_2nd/pwrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {001..003};
+for i in {001..027};
 do 
   cat header.in supercell-$i.in > NbSe2-$i.in;
-  #mpirun -np 4 pw.x <kk-$i.in> kk-$i.out;
+  mpirun -np 18 pw.x <kk-$i.in> kk-$i.out;
 done
 
