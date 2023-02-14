276e8025a0ec03efcc81212a49ad2087bfe1333a
honey_badger_td <47205931+hhoneybadger@users.noreply.github.com>
2023-02-14 22:09:55 +0600

    Update pwrun.sh

--- NbSe2_3rd/pwrun.sh
+++ NbSe2_3rd/pwrun.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-for i in {00001..00097};
+for i in {00001..00256};
 do 
   cat header.in supercell-$i.in > NbSe2-$i.in;
-  #mpirun -np 4 pw.x <kk-$i.in> kk-$i.out;
+  mpirun -np 18 pw.x <kk-$i.in> kk-$i.out;
 done
 
